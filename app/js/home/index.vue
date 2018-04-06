ff820069c59527322bc81ef2afc45184cbe4608f
fdeng <frank.deng@morningstar.com>
2018-04-06 10:57:36 +0800

    Home page silder.

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -1,14 +1,17 @@
 <template lang="html">
     <div>
         <Heador/>
+        <Slider/>
     </div>
 </template>
 
 <script>
 import Heador from "../public/header.vue"
+import Slider from "./hslider.vue"
 export default {
     components: {
         Heador,
+        Slider,
     },
 }
 </script>
