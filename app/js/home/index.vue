7d0d3eeac42a6c9e0ad51404276ea58eb43ec1a3
fdeng <frank.deng@morningstar.com>
2018-04-06 17:32:33 +0800

    footer and navbar.

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -7,17 +7,21 @@
         <Money/>
         <Product/>
         <Life/>
+        <HFooter/>
+        <Navbar/>
     </div>
 </template>
 
 <script>
 import Heador from "../public/header.vue"
+import Navbar from "../public/navbar.vue"
 import Slider from "./hslider.vue"
 import Novice from "./novice.vue"
 import Borrow from "./borrow.vue"
 import Money from "./money.vue"
 import Product from "./product.vue"
 import Life from "./life.vue"
+import HFooter from "./footer.vue"
 export default {
     components: {
         Heador,
@@ -27,6 +31,8 @@ export default {
         Money,
         Product,
         Life,
+        HFooter,
+        Navbar,
     },
 }
 </script>
